e42d9b0b5450a2205c7c7fcef65cfbb88771918e
ganeshi4u <ganeshvarmai4u@gmail.com>
2020-11-02 23:00:34 +0530

    Handle notification intent for community builds
    - Redirect to our website from notification
    - Update the api call
    - Redirect to changelog page on initiating download
    
    Change-Id: I621e0b6215975644dfe3507486607f927d036333

--- src/org/lineageos/updater/misc/Utils.java
+++ src/org/lineageos/updater/misc/Utils.java
@@ -150,6 +150,14 @@ public class Utils {
         return updates;
     }
 
+    public static String getArrowDownloadUrl(Context context) {
+        String device = SystemProperties.get(Constants.PROP_NEXT_DEVICE,
+                SystemProperties.get(Constants.PROP_DEVICE));
+        String arrowDownloadUrl = context.getString(R.string.arrow_download_url);
+
+        return arrowDownloadUrl.replace("{device}", device);
+    }
+
     public static String getServerURL(Context context) {
         //String incrementalVersion = SystemProperties.get(Constants.PROP_BUILD_VERSION_INCREMENTAL);
         String device = SystemProperties.get(Constants.PROP_NEXT_DEVICE,
@@ -404,5 +412,9 @@ public class Utils {
             case Constants.AUTO_UPDATES_CHECK_INTERVAL_MONTHLY:
                 return AlarmManager.INTERVAL_DAY * 30;
         }
+    }
+
+    public static boolean isCommunityBuild() {
+        return SystemProperties.get(Constants.PROP_RELEASE_TYPE).toLowerCase().equals("community");
     }
 }
